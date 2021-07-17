43b4b9b1576ac78adc005f206eeae4acc68f435b
Daniel Versoza Alves <daniel.ve3@gmail.com>
2021-07-17 19:53:14 -0300

    Added Repository decorators

--- backend/src/main/java/dvtech/dev/almoxcontrol/repositories/BarracaRepository.java
+++ backend/src/main/java/dvtech/dev/almoxcontrol/repositories/BarracaRepository.java
@@ -3,9 +3,11 @@ package dvtech.dev.almoxcontrol.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import dvtech.dev.almoxcontrol.models.Barraca;
 
+@Repository
 public interface BarracaRepository extends JpaRepository<Barraca, Integer> {
     void deleteBarracaByIdBarraca(Integer idBarraca);
 
--- backend/src/main/java/dvtech/dev/almoxcontrol/repositories/PessoaRepository.java
+++ backend/src/main/java/dvtech/dev/almoxcontrol/repositories/PessoaRepository.java
@@ -3,9 +3,11 @@ package dvtech.dev.almoxcontrol.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import dvtech.dev.almoxcontrol.models.Pessoa;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
     void deletePessoaByIdPessoa(Integer idPessoa);
 
